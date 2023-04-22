5b288a704417db869f77422e08e1cf80439925bd
Hano125-glitch <Hano125-glitch>
2023-04-21 19:09:28 -0700

    3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,8 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+putchar(65);
+putchar('\n');
+return (0);
 }
 
