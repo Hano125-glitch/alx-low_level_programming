e2f9e87aa51dbad2cdf80ac22e2ea192b38dc384
Hano125-glitch <Hano125-glitch>
2023-04-25 10:02:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
-#include<unistd.h>
+#include "main.h"
 /**
  * main - prints _putchar
  * Return: Always 0.
  */
 int main(void)
 {
-char str[] = "_putchar\n";
-int len = sizeof(str) / sizeof(str[0]);
-write(STDOUT_FILENO, str, len);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
